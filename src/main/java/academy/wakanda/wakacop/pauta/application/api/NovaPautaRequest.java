6c52bbb7872e8f97163147d2cc8fc213c960f185
OliveiraGomesMoises <mosesgdo@gmail.com>
2024-12-17 13:15:44 -0300

    configuracao aws sns and implemnets publisher

--- src/main/java/academy/wakanda/wakacop/pauta/application/api/NovaPautaRequest.java
+++ src/main/java/academy/wakanda/wakacop/pauta/application/api/NovaPautaRequest.java
@@ -1,11 +1,12 @@
 package academy.wakanda.wakacop.pauta.application.api;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.ToString;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.UUID;
+
 @Getter
 @ToString
 public class NovaPautaRequest {
